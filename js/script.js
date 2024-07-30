a694246dfb79056944e85117a033bbba1df10eca
armandotruglio <armando.truglio1@gmail.com>
2024-07-30 17:23:55 +0200

    Added Excercise steps

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+//Check JS connection
 console.log('JS OK')
+
+//Step dell'esercizio
+/*
+1. Recupero elementi dal DOM
+2. Preparo una variabile con la stringa '21'
+3. Chiedo all'utente il Nome
+4. Chiedo all'utente il Cognome
+5. Chiedo all'utente il suo colore preferito
+6. Genero la password sommando Nome + Cognome + Colore + '21'
+7. Inserisco la password nell'elemento del DOM*/
